33aa4be33a9d06fea9d3341f448403264880e815
Devaraj <devarajthemaker@gmail.com>
2026-06-11 15:53:44 +0530

    Fix icon size and update to Node 22

--- download-icon.js
+++ download-icon.js
@@ -10,10 +10,25 @@ const iconPath = path.join(__dirname, 'icon.png');
 
 if (!fs.existsSync(iconPath)) {
   console.log('Downloading Playwright icon...');
+  const iconTempPath = path.join(__dirname, 'icon-temp.png');
   https.get('https://raw.githubusercontent.com/microsoft/playwright-vscode/main/images/playwright-logo.png', res => {
     if (res.statusCode === 200) {
-      res.pipe(fs.createWriteStream(iconPath));
-      console.log('Icon downloaded successfully.');
+      const file = fs.createWriteStream(iconTempPath);
+      res.pipe(file);
+      file.on('finish', async () => {
+        file.close();
+        console.log('Icon downloaded successfully. Resizing to 512x512...');
+        try {
+          const sharp = (await import('sharp')).default;
+          await sharp(iconTempPath)
+            .resize(512, 512)
+            .toFile(iconPath);
+          fs.unlinkSync(iconTempPath);
+          console.log('Icon resized successfully!');
+        } catch (err) {
+          console.error('Failed to resize icon:', err);
+        }
+      });
     } else {
       console.error('Failed to download icon:', res.statusCode);
     }
